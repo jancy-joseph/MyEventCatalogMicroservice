cbb37d9c36f29a4be0bf44689c9eaefd7255d5f3
jancy-joseph <jancyworkmail@gmail.com>
2018-10-19 14:07:31 -0700

    ViewModel-class-init

---
+++ EventCatalogAPI/ViewModels/PaginatedItemsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventCatalogAPI.ViewModels
+{
+    public class PaginatedItemsViewModel<TEntity>
+        where TEntity:class
+    {
+        public int PageSize { get; private set; }
+        public int PageIndex { get; private set; }
+        public long Count { get; private set; }
+        public IEnumerable<TEntity> Data { get; set; }
+
+        public PaginatedItemsViewModel(int pageSize,int pageIndex,long count,IEnumerable<TEntity> data)
+        {
+            PageSize = pageSize;
+            PageIndex = pageIndex;
+            Count = count;
+            Data = data;
+        }
+    }
+}
